53843e949e17863df1e435a427138fea10ee9ad7
George Mandis <george.mandis@gmail.com>
2020-02-04 10:07:55 -0500

    Create server.js
    Added server file to run Express on Glitch

---
+++ server.js
@@ -0,0 +1,21 @@
+// Bubo RSS (on Glitch!)
+
+// init project
+const express = require("express");
+const app = express();
+
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("output"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/output/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
