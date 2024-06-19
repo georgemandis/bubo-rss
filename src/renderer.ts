39c6402253764ce8853b63e2e261c7df82feffc8
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-06-19 15:31:57 -0600

    Add a relative time formatter (#7)

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,13 +8,17 @@ import nunjucks from "nunjucks";
 const env: nunjucks.Environment = nunjucks.configure({ autoescape: true });
 import { readFile } from "node:fs/promises";
 import type { Feeds, JSONValue } from "./@types/bubo";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
+TimeAgo.addDefaultLocale(en);
+const timeFormatter = new TimeAgo("en-US");
 /**
  * Global filters for my Nunjucks templates
  */
-env.addFilter("formatDate", (dateString): string => {
+env.addFilter("relative", (dateString): string => {
 	const date: Date = new Date(Number.parseInt(dateString));
-	return !Number.isNaN(date.getTime()) ? date.toLocaleDateString() : dateString;
+	return !Number.isNaN(date.getTime()) ? timeFormatter.format(date) : dateString;
 });
 
 env.addFilter("formatTime", (dateString): string => {
