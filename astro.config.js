ce3d87d8e304b0a820b5d4296e99f7a0e3bdbb15
Carter McBride <18412686+carterworks@users.noreply.github.com>
2024-08-23 15:39:57 -0600

    Fix astro build to support gh-pages

--- astro.config.js
+++ astro.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://carterworks.github.io/",
+	base: "rss-reader",
 	output: "static",
 	integrations: [tailwindIntegration()],
 });
